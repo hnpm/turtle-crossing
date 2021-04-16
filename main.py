1ecaa0dfe8d69db9e924884f7053b29133dfa111
Hien Pham <phaminhien@hotmail.com>
2021-04-16 13:37:43 +0700

    create and move the turtle

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import time
 from turtle import Screen
+from player import Player
 
 screen = Screen()
+screen.title("Turtle Crossing Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
